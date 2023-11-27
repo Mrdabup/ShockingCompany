394d979ef4e22d54969175f413c0522dd851905f
Mrdabup <dabup123456@gmail.com>
2023-11-26 23:57:05 -0400

    Modified several stuff lol

--- LethalShock/Plugin.cs
+++ LethalShock/Plugin.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using BepInEx;
+using HarmonyLib;
 
 namespace LethalShock
 {
@@ -14,5 +15,17 @@ namespace LethalShock
         private const string modGUID = "MrdTika.LethalShock";
         private const string modName = "Lethal Shock";
         private const string modVersion = "inDev 1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static LethalShockBase Instance;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+        }
     }
 }
