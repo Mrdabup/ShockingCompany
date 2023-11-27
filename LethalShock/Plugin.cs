d497fced1a396591bc5bcf884f06d94521baae93
Mrdabup <dabup123456@gmail.com>
2023-11-27 00:23:12 -0400

    #Fixed issue with modVersion #Added LethalShockConf.cfg #We can finally boggy!

--- LethalShock/Plugin.cs
+++ LethalShock/Plugin.cs
@@ -6,19 +6,25 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using BepInEx;
 using HarmonyLib;
+using BepInEx.Configuration;
 
 namespace LethalShock
 {
     [BepInPlugin(modGUID, modName, modVersion)]
     public class LethalShockBase : BaseUnityPlugin
     {
+        //Names
         private const string modGUID = "MrdTika.LethalShock";
         private const string modName = "Lethal Shock";
-        private const string modVersion = "inDev 1";
+        private const string modVersion = "0.0.1";
 
         private readonly Harmony harmony = new Harmony(modGUID);
 
         private static LethalShockBase Instance;
+
+        private const string piName = "Lethal_Shock_CMD";
+
+        internal static ConfigFile CustomConfigFile { get; set; }
 
         void Awake()
         {
@@ -26,7 +32,13 @@ namespace LethalShock
             {
                 Instance = this;
             }
+            CustomConfigFile = new ConfigFile(Paths.ConfigPath + "\\LethalShockConf.cfg", true);
+            CustomConfigFile.Bind<string>("USERNAME", "Username", "JohnDoe", (ConfigDescription)null);
+            CustomConfigFile.Bind<string>("API", "ApiKey", "5c678926-d19e-4f86-42ad-21f5a76126db", (ConfigDescription)null);
+            CustomConfigFile.Bind<string>("SHARECODE", "Code", "17519CD8GAP", (ConfigDescription)null);
             harmony.PatchAll(typeof(LethalShockBase));
+
+
         }
     }
 }
