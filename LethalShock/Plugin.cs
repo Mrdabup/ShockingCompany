9e67a1d20f366bb18eb4925a0650b8add4075730
Tika <kinectika@togen.xyz>
2023-11-28 12:54:28 -0600

    Add Intensity and Duration variables

--- LethalShock/Plugin.cs
+++ LethalShock/Plugin.cs
@@ -4,6 +4,7 @@ using HarmonyLib;
 using UnityEngine;
 using System;
 using System.Net.Http;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,27 +16,32 @@ namespace LethalShock
         private const string modGUID = "MrdTika.LethalShock";
         private const string modName = "Lethal Shock";
         private const string modVersion = "0.0.1";
-
+    
         private readonly Harmony harmony = new Harmony(modGUID);
         private static LethalShockBase Instance;
         private const string Name = "Lethal_Shock_CMD";
-
+        
+        private int Intensity;
+        private  int Duration;
         private static ConfigEntry<string> Username;
         private static ConfigEntry<string> ApiKey;
         private static ConfigEntry<string> Code;
 
-        void Awake()
+       private void Awake()
         {
             if (Instance == null)
             {
                 Instance = this;
             }
-
+            
             // Bind configuration values using separate ConfigEntry instances
             Username = Config.Bind("Settings", "Username", "JohnDoe", "Your username");
             ApiKey = Config.Bind("Settings", "ApiKey", "5c678926-d19e-4f86-42ad-21f5a76126db", "Your API key");
             Code = Config.Bind("Settings", "Code", "17519CD8GAP", "Your share code");
-
+            
+            Intensity = 100; //placeholder numbers, Has to be between 1 and 100
+            Duration = 5; //placeholder numbers, Has to be between 1 and 15
+            
             harmony.PatchAll(typeof(LethalShockBase));
             // Commenting the line below assuming CheckPlayer is not defined in this script.
             // harmony.PatchAll(typeof(CheckPlayer));
@@ -50,7 +56,7 @@ namespace LethalShock
             using (HttpClient client = new HttpClient())
             {
                 // Prepare the JSON payload
-                string jsonPayload = $"{{\"Username\":\"{Username.Value}\",\"Name\":\"TG_Bot_Script\",\"Code\":\"{Code.Value}\",\"Intensity\":\"6\",\"Duration\":\"1\",\"Apikey\":\"{ApiKey.Value}\",\"Op\":\"0\"}}";
+                string jsonPayload = $"{{\"Username\":\"{Username.Value}\",\"Name\":\"{name}\",\"Code\":\"{Code.Value}\",\"Intensity\":\"{Intensity},\"Duration\":\"{Duration}\",\"Apikey\":\"{ApiKey.Value}\",\"Op\":\"0\"}}";
 
                 // Create a StringContent with the JSON payload and set content type
                 StringContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
